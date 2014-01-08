6e68e234d37a8cb73f78bdc85e26b53e0d2b5e74
Andrew Meakovski <meako689@gmail.com>
2014-01-08 17:44:31 +0200

    ticket:4: Wrote test, context processor and template

--- hello42/settings.py
+++ hello42/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import sys
 import os
+
+import django.conf.global_settings as DEFAULT_SETTINGS
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_PATH = os.path.join(BASE_DIR, 'hello42')
 
@@ -53,6 +56,10 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = DEFAULT_SETTINGS.TEMPLATE_CONTEXT_PROCESSORS + (
+    'watchr.context_processors.settings_context',
 )
 
 ROOT_URLCONF = 'hello42.urls'
