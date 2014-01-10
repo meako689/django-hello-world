9a9b9d4ecc438cbfa9b718cdde1a9692a947697c
Andrew Meakovski <meako689@gmail.com>
2014-01-10 23:10:09 +0200

    ticket:9 Include deferred models

--- hello42/hello/management/commands/listmodels.py
+++ hello42/hello/management/commands/listmodels.py
@@ -1,11 +1,11 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.db import models
 import sys
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        for model in models.get_models():
+        for model in models.get_models(include_deferred=True):
             msg= "{name}:{count}\n".format(
                     name=model.__name__,
                     count=model.objects.count()
