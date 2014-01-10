55d7bd604cb227a4b47b4d22dc0835ea001ea953
Andrew Meakovski <meako689@gmail.com>
2014-01-11 00:06:03 +0200

    Added admin to monitor watchet signals

--- hello42/watchr/admin.py
+++ hello42/watchr/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from models import RecordedRequest, ModelChangeRecord
 
-# Register your models here.
+admin.site.register(RecordedRequest)
+admin.site.register(ModelChangeRecord)
